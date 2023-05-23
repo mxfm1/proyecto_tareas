2a4c498615f91c0ba23910c666e425145db720b2
felipe <fandmxa@gmail.com>
2023-05-22 20:20:58 -0400

    IND4 Actualizacion Tareas

--- main/users/urls.py
+++ main/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import registerPage,loginPage,home,logoutUser,tareas,CrearTarea, DetalleTarea
+from .views import registerPage,loginPage,home,logoutUser,tareas,CrearTarea, DetalleTarea,ActualizarTarea
 
 urlpatterns = [
     path('home/',home,name='home'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('tareas/',tareas,name='tareas'),
     path('tareas/crear',CrearTarea.as_view(),name='crear_tarea'),
     path('tareas/<int:pk>',DetalleTarea.as_view(),name='detalle_tarea'),
+    path('tareas/actualizar/<int:pk>',ActualizarTarea.as_view(),name='actualizar_tarea'),
 ]
--- main/users/views.py
+++ main/users/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic.edit import CreateView 
 from django.views.generic.detail import DetailView
+from django.views.generic import UpdateView
 from .models import Tareas,User
 
 from .forms.forms import CreateUserForm,LoginForm,CrearTareasForm
@@ -25,16 +26,16 @@ def home(request):
     tipo_tareas = request.GET.get('tipo_tareas')
     user = request.user.id
     
-    tareas = Tareas.objects.filter(usuario_id=user,estado__estado='pendiente').order_by('fecha_vencimiento')
+    tareas = Tareas.objects.filter(usuario_id=user).order_by('fecha_vencimiento')
     
-    # if tipo_tareas == 'todas':
-    #     tareas = Tareas.objects.filter(usuario_id=user).order_by('-fecha_vencimiento')    
-    # if tipo_tareas == 'pendiente':
-    #     tareas = tareas.filter(estado__estado='pendiente').order_by('-fecha_vencimiento')
-    # elif tipo_tareas == 'en progreso':
-    #     tareas = Tareas.objects.filter(usuario_id=user,estado__estado='en progreso')
-    # elif tipo_tareas == 'completado':
-    #     tareas = Tareas.objects.filter(usuario_id=user,estado__estado='completado')
+    if tipo_tareas == 'todas':
+        tareas = Tareas.objects.filter(usuario_id=user).order_by('-fecha_vencimiento')    
+    if tipo_tareas == 'pendiente':
+        tareas = tareas.filter(usuario_id=user,estado__estado='pendiente').order_by('-fecha_vencimiento')
+    elif tipo_tareas == 'en progreso':
+        tareas = Tareas.objects.filter(usuario_id=user,estado__estado='en progreso').order_by('-fecha_vencimiento')
+    elif tipo_tareas == 'completada':
+        tareas = Tareas.objects.filter(usuario_id=user,estado__estado='completada').order_by('-fecha_vencimiento')
     context = {'tareas':tareas}
     return render(request, 'accounts/home.html', context=context)
 
@@ -105,6 +106,7 @@ def tareas(request):
 #         print(f'request: {self.request.user}')
 #         return super().form_valid(form)
 
+# Vistas de Tareas
 class CrearTarea(LoginRequiredMixin,CreateView):
     model = Tareas
     form_class = CrearTareasForm
@@ -114,4 +116,13 @@ class CrearTarea(LoginRequiredMixin,CreateView):
     def form_valid(self,form):
         form.instance.usuario = self.request.user
         return super().form_valid(form)
+
+class ActualizarTarea(LoginRequiredMixin,UpdateView):
+    model = Tareas
+    form_class = CrearTareasForm
+    template_name = 'accounts/crear_tarea.html'
+    success_url = reverse_lazy('home')
     
+    def form_valid(self,form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)    
